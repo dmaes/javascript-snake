89a2f4e4a444eb6458e5a78389ac0e5f1dbf3c52
Dieter Maes <dieter.maes@dmaes.be>
2018-04-22 22:32:07 +0200

    remove code that students must implement

--- student.js
+++ student.js
@@ -5,10 +5,10 @@ maak de volgende variabelen
 'interests' - Een rij met je interesses in als strings
 'show' - Een boolean die aangeeft of je deze info al dan niet wilt laten zien
  */
-var name = "Dieter Maes";
-var age = 19;
-var interests = ["Coputers", "Music", "Food"];
-var show = true;
+
+
+
+
 
 /*
 function isAllowedDirection
@@ -19,25 +19,9 @@ kijk of een nieuwe richting toegestaan is.
     - nieuwe richting mag niet tegengesteld zijn aan houdige richting
  */
 
-function isAllowedDirection(direction, keywordDirection) {
-    var result;
 
-    if (direction === keywordDirection) {
-        result = false;
-    } else if (direction === "up" && keywordDirection === "down") {
-        result = false;
-    } else if (direction === "down" && keywordDirection === "up") {
-        result = false;
-    } else if (direction === "left" && keywordDirection === "right") {
-        result = false;
-    } else if (direction === "right" && keywordDirection === "left") {
-        result = false;
-    } else {
-        result = true;
-    }
 
-    return result;
-}
+
 
 /*
 function snakeContainsCell
@@ -50,12 +34,8 @@ snake.cells is een rij.
 row en col van een cell worden verkregen met cell.row en cell.col
  */
 
-function snakeContainsCell(snake, row, col) {
-    var result = false;
-    for (var i = 0; i < snake.cells.length; i++) {
-        var cell = snake.cells[i];
-        if (cell.row === row && cell.col === col) result = true;
-    }
-    return result;
-}
+
+
+
+
 
